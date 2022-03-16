5f006a3bbef57b6ffd475cb2528c3e086ac72511
nankaine <94109329+nankaine@users.noreply.github.com>
2022-03-16 16:38:26 +0800

    添加
    测试

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#src-link custom /usr/src/openwrt/custom-feed
+src-git kenzo https://github.com/kenzok8/openwrt-packages >>feeds.conf.default
+src-git passwall https://github.com/xiaorouji/openwrt-passwall >>feeds.conf.default
+src-git small https://github.com/kenzok8/small >>feeds.conf.default
