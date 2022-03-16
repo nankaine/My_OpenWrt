7d341813f0201bd0e911651d874df9596fcd19b7
nankaine <94109329+nankaine@users.noreply.github.com>
2022-03-16 16:46:16 +0800

    Update diy-part1.sh
    修改格式

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,7 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #src-link custom /usr/src/openwrt/custom-feed
-src-git kenzo https://github.com/kenzok8/openwrt-packages >>feeds.conf.default
-src-git passwall https://github.com/xiaorouji/openwrt-passwall >>feeds.conf.default
-src-git small https://github.com/kenzok8/small >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
